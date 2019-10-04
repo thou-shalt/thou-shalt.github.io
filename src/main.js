008b4da922d499959e395ac1d1e6221caa4749ea
Thou Shalt <thou-shalt@software-materialims.org>
2019-10-04 17:28:52 +0100

    thou shalt build

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import p5 from './p5.min.js';
 
-let thouShalt = new p5(( sketch ) => {
+new p5((sk) => {
+  let font; 
+  sk.preload = () => {
+    font = sk.loadFont('assets/OCR-ABT.otf');
+  };
+  sk.setup = () => {
+    let cnv = sk.createCanvas(window.innerWidth, window.innerHeight);
+    cnv.canvas.style.display = 'block';
+    // console.log(cnv.canvas.style);
+  };
 
-    sketch.setup = () => {
-      sketch.createCanvas(window.outerWidth, window.outerHeight);
-    };
-  
-    sketch.draw = () => {
-      sketch.background(2,3,5);
-    };
-  }, 'thou-shalt');
+  sk.draw = () => {
+    sk.background(200, 233, 205);
+    // sk.background(2, 23, 25);
+    sk.textSize(sk.height/4);
+    sk.textFont(font);
+    sk.fill(12, 43, 45);
+    sk.textAlign(sk.CENTER);
+    sk.text('THOU SHALT', sk.width/2,sk.height/2);
+   };
+
+  sk.windowResized = () => {
+    sk.resizeCanvas(window.innerWidth, window.innerHeight);
+  };
+}, 'thou-shalt');
