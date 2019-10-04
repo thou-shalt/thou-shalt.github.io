359496842bc9a633db50ac20f5ace39d0f84df53
Thou Shalt <thou-shalt@software-materialims.org>
2019-10-04 15:24:19 +0100

    size

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ import p5 from './p5.min.js';
 let thouShalt = new p5(( sketch ) => {
 
     sketch.setup = () => {
-      sketch.createCanvas(sketch.displayWidth, sketch.displayHeight);
+      sketch.createCanvas(window.outerWidth, window.outerHeight);
     };
   
     sketch.draw = () => {
-      sketch.fullscreen(); 
       sketch.background(2,3,5);
     };
   }, 'thou-shalt');
